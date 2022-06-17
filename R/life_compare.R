7a8a9487044a9a7741754402f05970ac634313ea
Xuezhixing Zhang <xuezhixing.zhang@yale.edu>
2022-06-16 21:23:43 -0400

    updated

---
+++ R/life_compare.R
@@ -0,0 +1,207 @@
+#' @title Compare life expectancies
+#' @description  A Function for comparing life expectancies of each subgroup. By default, this function will calculate the proportion of samples that have a larger life expectancy or life expectancy proportion in your reference group than other groups.
+#' @param status.include The status we aim to compare. It can be a number or a vector. Default is 0, which means we'll consider all status.
+#' @param ref.var A vector that contains all covariates used as reference variables when comparing each subgroup.
+#' @param file_path The file path for reading data.
+#' @param file The file path for outputs.
+#' @param states The total number of transition states in our data.
+#' @param ref.level A vector that declares the reference value of each reference variable.
+#' @param index.matrix A matrix that generated in \code{mlifeTable_plot()}. You don't need to specify it when using \code{mlifeTable_plot()}.
+#' @param status.names A vector used to specify names of each status except death. 
+#' @param prop The indicator for proportion plots and tables. If TRUE, this function will output life expectancy proportion plots and tables in addition to orginal life expectancy plots. Default is TRUE. 
+#' @param criteria The criteria for comparison, which can be either ">" or "<". Default is ">".
+#' @import utils
+#' @export
+#' @return A \code{.csv} file.
+#' @examples
+#' \dontrun{
+#' 
+#' #By setting the parameter 'compare' in mlifeTable_plot() to TRUE. We can directly use this function.
+#' mlifeTable_plot(X=lifedata[,-1],status.include = 3,
+#'       groupby = c("male","norcg","cogcg","demcg"), 
+#'       cred = 0.92, 
+#'       states = 9,
+#'       file_path = ".",
+#'       compare = TRUE,
+#'       ref.var = c("norcg","cogcg","demcg"),
+#'       ref.level = c(0,0,0))
+#' 
+#'}
+
+life_compare <- function(file_path,file,
+                         status.include = 0 , states,
+                         ref.var, ref.level, index.matrix,
+                         prop = TRUE,
+                         criteria = ">",
+                         status.names = NA){
+  UseMethod("life_compare")
+}
+
+#' @export
+##Construct a function for comparsion
+life_compare <- function(file_path,file,
+                         status.include = 0 , states,
+                         ref.var, ref.level, index.matrix,
+                         prop = TRUE,
+                         criteria = ">",
+                         status.names = NA){
+  #Initialize
+  other.var <- colnames(index.matrix)[which(!(colnames(index.matrix) %in% ref.var))]
+  if(length(other.var) == 0){Index.other <- matrix("",nrow=1, ncol=1)}
+  else{Index.other <- unique(as.matrix(index.matrix[,other.var]))}
+
+  Index.ref <- unique(as.matrix(index.matrix[,ref.var]))
+  
+  if(status.include [1] ==0 | is.na(status.include [1])){
+    
+    if(prop == TRUE){
+      table <- matrix(nrow =2*states-1,ncol=dim(Index.other)[1]*(dim(Index.ref)[1]-1))
+      if(is.na(status.names[1])){
+        row.names <- c("Total Life", paste("Status=",1:(states-1),sep=''),paste("Status=",1:(states-1),"_prop",sep=''))
+      }
+      else{
+        row.names <- c("Total Life", c(status.names),paste(c(status.names),"_prop",sep=''))
+      }
+    }
+    else{
+      table <- matrix(nrow =states,ncol=dim(Index.other)[1]*(dim(Index.ref)[1]-1))
+      if(is.na(status.names[1])){
+        row.names <- c("Total Life", paste("Status=",1:(states-1),sep=''))
+      }
+      else{
+        row.names <- c("Total Life", paste(c(status.names),sep=''))
+      }
+    }
+    col.names <- NULL
+  }
+  else{
+    table <- matrix(nrow =length(status.include),ncol=dim(Index.other)[1]*(dim(Index.ref)[1]-1))
+    if(is.na(status.names[1])){
+      row.names <- paste("Status=",status.include ,sep='')
+    }
+    else{
+      temp <- c(status.names,"death")
+      row.names <- paste(temp[status.include],sep='')
+    }
+    col.names <- NULL
+  }
+  
+  for(i in 1:dim(Index.ref)[1]){
+    if(all(Index.ref[i,] == ref.level)){ref.level.num <- i}
+    else{next}
+  }
+  
+  Index <- as.matrix(Index.ref[-ref.level.num, ])
+  
+  #Calculate
+  for(i in 1:dim(index.matrix)[1]){
+    
+    if(!all(index.matrix[i,ref.var] == ref.level)){next}
+    else{
+      #Find current reference index
+      index.ref <- paste(index.matrix[i,], collapse = '')
+      index.other <- index.matrix[i,other.var]
+      
+      #Find index.other.num
+      for(j in 1:dim(Index.other)[1]){
+        if(all(Index.other[j,] == index.other)){index.other.num <- j}
+        else{next}
+      }
+      
+      
+      for(j in 1:dim(Index)[1]){
+        
+        #Find index.com
+        for(k in 1:dim(index.matrix)[1]){
+          if(all(index.matrix[k,ref.var] == Index[j,]) &
+             all(index.matrix[k,other.var] == index.other)){
+            
+            index.com <- paste(index.matrix[k,],collapse = '')
+            break
+          }
+          else{next}
+        }
+        
+        table[,j+dim(Index)[1]*(index.other.num-1)] <- life_compare_one(file_path, file,
+                                                                        status.include , states,
+                                                                        index.ref,
+                                                                        index.com,
+                                                                        criteria = criteria,
+                                                                        prop = prop)
+        col.names[j+dim(Index)[1]*(index.other.num-1)] <- paste("group",index.com,sep = '')
+      }
+      
+    }
+  }
+  
+  rownames(table) <- row.names
+  colnames(table) <- col.names
+  
+  utils::write.csv(table, file = paste(file, "/Compare.csv",sep=''))
+  
+}
+
+#This function will compare the life expectancy to one specific group.
+life_compare_one <- function(file_path,file,
+                             status.include , states,
+                             index.ref,index.com,
+                             criteria = ">",prop){
+  
+  value <- NULL
+  
+  file_name.ref <- paste(paste(file_path,"/lifetable",sep=''),
+                         index.ref,
+                         ".txt",
+                         sep="")
+  file_name.com <- paste(paste(file_path,"/lifetable",sep=''),
+                         index.com,
+                         ".txt",
+                         sep="")
+  
+  data.ref <- utils::read.table(file_name.ref,header = TRUE)
+  data.com <- utils::read.table(file_name.com,header = TRUE)
+  
+  if(status.include [1] ==0 | is.na(status.include [1])){
+    
+    
+    
+    ttle.ref <- rowSums(data.ref[,1:(states-1)])
+    ttle.com <- rowSums(data.com[,1:(states-1)])
+    
+    #Find Proportion of expectancy years
+    for(i in 1:(states)){
+      if(criteria == ">"){
+        if(i == 1){value[i] <- mean(as.vector(ttle.ref)>as.vector(ttle.com))}
+        else{
+          value[i] <- mean(as.vector(data.ref[,i-1])>as.vector(data.com[,i-1]))
+        }
+      }
+      else if(criteria == "<"){
+        if(i == 1){value[i] <- mean(as.vector(ttle.ref)<as.vector(ttle.com))}
+        else{
+          value[i] <- mean(as.vector(data.ref[,i-1])<as.vector(data.com[,i-1]))
+        }
+      }
+      
+    }
+    #Find Proportion of expectancy years proportions
+    if(prop == TRUE){
+      for(i in 1:(states-1)){
+        if(criteria == ">"){
+          value[states + i] <- mean(as.vector(data.ref[,i]/ttle.ref)>as.vector(data.com[,i]/ttle.com))
+        }
+        else if(criteria == "<"){
+          value[states + i] <- mean(as.vector(data.ref[,i]/ttle.ref)<as.vector(data.com[,i]/ttle.com))
+        }
+      }
+    }
+  }
+  else{
+    for(i in 1:length(status.include )){
+      if(criteria == ">"){value[i] <- mean(as.vector(data.ref[,status.include [i]])>as.vector(data.com[,status.include [i]]))}
+      else if(criteria == "<"){value[i] <- mean(as.vector(data.ref[,status.include [i]])<as.vector(data.com[,status.include[i]]))}
+    }
+  }
+
+  return(value)
+}
---
+++ R/mlifeTable_plot.R
@@ -0,0 +1,336 @@
+#' @title Plot life expectancies
+#' @description  A Function for posterior means and credible intervals plots.
+#' @param status.include The status we aim to calculate. It can be a number or a vector. Default is 0, which means we'll consider all status. 
+#' @param groupby A vector that contains all covariates used for making subgroups.
+#' @param file_path The file path for outputs.
+#' @param X Covariates including age. It should be a data.frame or a matrix.
+#' @param cred credible level. e.g. if \code{cred = 0.92}, we will get the 92\% credible interval.
+#' @param states The total number of transition states in our data.
+#' @param prop The indicator for proportion plots and tables. If TRUE, this function will output life expectancy proportion plots and tables in addition to orginal life expectancy plots. Default is TRUE. 
+#' @param subgroup.names A vector that contains names of each subgroup. You can also specify them in the output files.
+#' @param compare An indicator for generating comparsion results. If TRUE, this function will quote \code{life_compare()} and generate a table with all comparsion results based on the reference variables and reference levels you defined.
+#' @param status.names A vector used to specify names of each status except death. 
+#' @param ... Extra parameters for \code{life_compare()}. For details, please use \code{?life_compare()}.
+#' @import ggplot2 utils stats
+#' @export
+#' @return Plots and tables for posterior means and credible intervals of each subgroups
+#' @examples
+#' \dontrun{
+#' 
+#' mlifeTable_plot(X=lifedata[,-1],status.include = 3,
+#'       groupby = c("male","norcg","cogcg","demcg"), 
+#'       cred = 0.92, 
+#'       states = 9,
+#'       file_path = ".")
+#'       
+#' }
+
+mlifeTable_plot <- function(status.include = 0,
+                            groupby,
+                            file_path,
+                            X,
+                            cred=0.92,
+                            states,
+                            prop = TRUE,
+                            subgroup.names =NULL,
+                            status.names=NA,
+                            compare=FALSE,
+                            ...){
+  UseMethod("mlifeTable_plot")
+}
+
+#' @export
+mlifeTable_plot.default <- function(status.include = 0,
+                                    groupby,
+                                    file_path,
+                                    X,
+                                    cred=0.92,
+                                    states,
+                                    prop = TRUE,
+                                    subgroup.names =NULL,
+                                    status.names=NA,
+                                    compare=FALSE,
+                                    ...){
+  
+  file <- paste(file_path,"/mplotResults",sep='')
+  dir.create(file)
+  #Output Names
+  
+  if(is.na(status.names[1])){
+    if(status.include[1] == 0 | is.na(status.include[1])){output.names <- c(1:(states-1))}
+    else{output.names <- status.include}
+  }
+  else{
+    if(status.include[1] == 0 | is.na(status.include[1])){output.names <- status.names}
+    else{output.names <- status.names[status.include]}
+  }
+  ##Subgroup
+  age.index <- which(colnames(as.data.frame(X)) %in% "age" ==TRUE)
+  X <- X[,-age.index]
+  data <- as.data.frame(X)
+  cols <- colnames(data)
+  vars.group <- groupby
+  vars.other <- setdiff(cols,groupby)
+  
+  ##Construct index matrix
+  if(!is.na(vars.group[1])){
+    if(length(vars.group)>1){
+      data.group.list <- lapply(data[,vars.group],c)
+      index.matrix <- sapply(unique(data[,vars.group]),as.character)
+    }
+    else{
+      data.group.list <- list(data[,vars.group])
+      names(data.group.list) <- vars.group
+      index.matrix <- as.matrix(sapply(unique(data[,vars.group]),as.character))
+      colnames(index.matrix) <- vars.group
+    }
+    
+  }
+
+  index.dim <- ifelse(is.null(dim(index.matrix)[1]),1,dim(index.matrix)[1])
+  
+  total <- matrix(nrow=index.dim,ncol=3)
+  
+  ##For general population
+  
+  if(status.include[1] ==0 | is.na(status.include[1])){
+    
+    ##Construct lists for different status
+    life.list <- list()
+    life.list.prop <- list()
+    for(i in 1:(states-1)){
+      
+      temp <- list(matrix(nrow=index.dim,ncol=3))
+      names(temp) <- paste("status","=",i,collapse = '')
+        
+      life.list <- append(life.list,temp)
+      life.list.prop <- append(life.list.prop,temp)
+      
+    }
+    
+    ##Tables and plots for each subgroup
+    for(i in 1:index.dim){
+      
+      file_name <- paste(paste(file_path,"/lifetable",sep=''),
+                         ifelse(is.null(dim(index.matrix)[1]),
+                                1,
+                                paste(index.matrix[i,],collapse = '')),
+                         ".txt",
+                         sep="")
+      
+      data <- utils::read.table(file_name, header = TRUE)
+      colnum <- dim(data)[2]
+      index <- index.matrix[i,]
+      
+      ##Total life expectancy
+      ttle <- rowSums(data[,1:(colnum-1)])
+      row1 <- c(mean(ttle),stats::quantile(ttle, c(1-cred, cred)))
+      total[i,] <- row1
+      
+      ##Calculate posterior means and credible intervals for each status
+      for(j in 1:(states-1)){
+        life.list[[j]][i,] <- c(mean(data[,j]),stats::quantile(data[,j], c(1-cred, cred)))
+        life.list.prop[[j]][i,] <- c(mean(data[,j]/ttle),stats::quantile(data[,j]/ttle, c(1-cred, cred)))
+      }
+    }
+    
+    life.list <- append(list("Total" = total),life.list)
+      
+    ##Save plots and tables
+    tables_plots_1(index.dim,
+                   index.matrix,
+                   states,
+                   life.list,
+                   life.list.prop,
+                   file,
+                   subgroup.names,
+                   prop=prop,
+                   output.names)
+    
+  }
+  
+  ##For one or several given status
+  else{
+    life.list <- list()
+    life.list.prop <- list()
+    for(j in status.include){
+      
+      temp <- list(matrix(nrow=index.dim,ncol=3))
+      names(temp) <- paste("status","=",j,collapse = '')
+      
+      life.list <- append(life.list,temp)
+      
+    }
+    
+    ##Tables and plots for each subgroup
+    for(i in 1:index.dim){
+      
+      file_name <- paste(paste(file_path,"/lifetable",sep=''),
+                         ifelse(is.null(dim(index.matrix)[1]),
+                                1,
+                                paste(index.matrix[i,],collapse = '')),
+                         ".txt",
+                         sep="")
+      
+      data <- utils::read.table(file_name,header = TRUE)
+      colnum <- dim(data)[2]
+      index <- index.matrix[i,]
+      
+      
+      ##Calculate posterior means and credible intervals for each status
+      for(j in 1:length(status.include)){
+        life.list[[j]][i,] <- c(mean(data[,j]),stats::quantile(data[,j], c(1-cred, cred)))
+      }
+      
+    }
+    
+    ##Save plots and tables
+    tables_plots_2(index.dim,
+                   index.matrix,
+                   status.include,
+                   life.list,
+                   file,
+                   subgroup.names,
+                   output.names)
+  }
+  if(compare == TRUE){life_compare(file_path,
+                                   file,
+                                   status.include = status.include, 
+                                   states = states,
+                                   status.names = status.names,
+                                   ...,
+                                   index.matrix = index.matrix, 
+                                   prop = prop)
+    }
+  
+}
+
+##Function for tables and plots when status = 0.
+tables_plots_1 <- function(index.dim,index.matrix,states,
+                         life.list,life.list.prop,
+                         file,
+                         subgroup.names,
+                         prop,
+                         output.names){
+  ##Creat subgroup names using index.matrix
+  if(is.null(subgroup.names[1])){
+    subgroup.names <- NULL
+    for(i in 1:index.dim){
+      subgroup.names[i] <- paste("group",paste(index.matrix[i,],collapse=""),sep="")
+    }
+  }
+
+  
+  ##Write total life expectancy
+  temp <- data.frame(life.list[[1]],"subgroup" = subgroup.names)
+  colnames(temp) <- c("mean", "left.bound","right.bound","subgroup")
+
+  
+  ##Tables
+  utils::write.table(x=temp,
+                   file = paste(paste(file,"/total_life_expectancy",sep=''),
+                                ".txt",
+                                sep=""),
+                   row.names = FALSE)
+  ##Plots
+  p <- ggplot2::ggplot(data = temp, 
+                       ggplot2::aes_string(x = 'mean', y = 'subgroup', xmin = 'left.bound', xmax = 'right.bound')) +
+    ggplot2::geom_point() +
+    ggplot2::geom_errorbarh(height = 0) +
+    ggplot2::theme_light() + 
+    ggplot2::labs(x = "Total extectancy of remaining life", y = "Subgroup", title = "Total life expectancy for each subgroup")
+  ggplot2::ggsave(paste(paste(file,"/total_life_expectancy",sep=''),
+               ".png",
+               sep=""),
+         width=10,height = 6.18,units="in")
+  
+  for(i in 1:(states-1)){
+    ##Life extectancy
+    temp1 <- data.frame(life.list[[i+1]],"subgroup" = subgroup.names)
+    colnames(temp1) <- c("mean", "left.bound","right.bound","subgroup")
+
+    
+    utils::write.table(x=temp1,
+                     file = paste(paste(file,"/life_expectancy_","status_",output.names[i],sep=''),
+                                  ".txt",
+                                  sep=""),
+                     row.names = FALSE)
+    p <- ggplot2::ggplot(data = temp1, 
+                         ggplot2::aes_string(x = 'mean', y = 'subgroup', xmin = 'left.bound', xmax = 'right.bound')) +
+      ggplot2::geom_point() +
+      ggplot2::geom_errorbarh(height = 0) +
+      ggplot2::theme_light() + 
+      ggplot2::labs(x = "Extectancy years of remaining life", y = "Subgroup", title = "Total life expectancy for each subgroup")  
+    ggplot2::ggsave(paste(paste(file,"/life_expectancy_","status_",output.names[i],sep=''),
+                 ".png",
+                 sep=""),
+           width=10,height = 6.18,units="in")
+    if(prop == TRUE){
+      ##Life extectancy proportion
+      temp2 <- data.frame(life.list.prop[[i]],"subgroup" = subgroup.names)
+      colnames(temp2) <- c("mean", "left.bound","right.bound","subgroup")
+      
+      
+      utils::write.table(x=temp2,
+                         file = paste(paste(file,"/life_expectancy_prop_","status_",output.names[i],sep=''),
+                                      ".txt",
+                                      sep=""),
+                         row.names = FALSE)
+      p <- ggplot2::ggplot(data = temp2, 
+                           ggplot2::aes_string(x = 'mean', y = 'subgroup', xmin = 'left.bound', xmax = 'right.bound')) +
+        ggplot2::geom_point() +
+        ggplot2::geom_errorbarh(height = 0) +
+        ggplot2::theme_light() + 
+        ggplot2::labs(x = "Life Extectancy proportion of remaining life", y = "Subgroup", title = "Life Extectancy proportion for each subgroup")  
+      ggplot2::ggsave(paste(paste(file,"/life_expectancy_prop_","status_",output.names[i],sep=''),
+                            ".png",
+                            sep=""),
+                      width=10,height = 6.18,units="in")
+    }
+  }
+}
+##Function for tables and plots when status.include != 0.
+tables_plots_2 <- function(index.dim,index.matrix,status.include,
+                           life.list,
+                           file,
+                           subgroup.names,
+                           output.names){
+  
+  ##Creat subgroup names using index.matrix
+  if(is.null(subgroup.names[1])){
+    subgroup.names <- NULL
+    for(i in 1:index.dim){
+      subgroup.names[i] <- paste("group",paste(index.matrix[i,],collapse=""),sep="")
+    }
+  }
+  
+  ##Write total life expectancy
+  temp <- data.frame(life.list[[1]],"subgroup" = subgroup.names)
+  colnames(temp) <- c("mean", "left.bound","right.bound","subgroup")
+
+  
+  for(i in 1:length(status.include)){
+    ##Life extectancy
+    temp1 <- data.frame(life.list[[i]],"subgroup" = subgroup.names)
+    colnames(temp1) <- c("mean", "left.bound","right.bound","subgroup")
+
+    
+    utils::write.table(x=temp1,
+                     file = paste(paste(file,"/life_expectancy_","status_",output.names[i],sep=''),
+                                  ".txt",
+                                  sep=""),
+                     row.names = FALSE)
+    p <- ggplot2::ggplot(data = temp1, 
+                         ggplot2::aes_string(x = 'mean', y = 'subgroup', xmin = 'left.bound', xmax = 'right.bound')) +
+      ggplot2::geom_point() +
+      ggplot2::geom_errorbarh(height = 0) +
+      ggplot2::theme_light() +
+      ggplot2::labs(x = "Extectancy years of remaining life", y = "Subgroup", title = "Total life expectancy for each subgroup")  
+    ggplot2::ggsave(paste(paste(file,"/life_expectancy_","status_",output.names[i],sep=''),
+                 ".png",
+                 sep=""),
+           width=10,height = 6.18,units="in")
+  }
+  
+}
+
