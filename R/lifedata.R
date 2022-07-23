38caa51ac8947c218bc822f2b88308e7e7f4b71b
Xuezhixing Zhang <xuezhixing.zhang@yale.edu>
2022-07-23 02:11:02 -0400

    updated

--- R/lifedata.R
+++ R/lifedata.R
@@ -3,11 +3,11 @@
 #' Data extracted and processed from The Health and Retirement Study (HRS). 
 #' 
 #' Each subject has 3 status in the cohort: 1: health; 2: unhealthiness; 3: death. 
-#' Thus we will have 4 kind of possible transitions: 1:health to unhealthiness; 2: health to death; 3: unhealthiness to health; 4: unhealthiness to death. To check the transition status for each subject, please use \code{lifedata[,1]}.
+#' Thus we will have 6 kind of possible transitions: 1:health to health; 2:health to unhealthiness; 3: health to death; 4: unhealthiness to health; 5: unhealthiness to unhealthiness; 6: unhealthiness to death. To check the transition status for each subject, please use \code{lifedata[,1]}.
 #' 
 #' To use this package on your data, please make sure your data have a transition status vector.
 #'
-#' @format A data frame with 6699 rows and 16 variables:
+#' @format A data frame with 8015 rows and 16 variables:
 #' \describe{
 #'   \item{trans}{Transition status was recorded in the original data. In this data, we have 4 kinds of transtions in total.}
 #'   \item{age}{Age for each subject.}
--- data-raw/lifedata_load.R
+++ data-raw/lifedata_load.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-lifedata<- read.csv("./lifedata.csv") %>%
+lifedata<- read.csv("./lifedata.csv")[1:8474,] %>%
   mutate(black = ifelse(black == 1 & hispanic ==0,1,
                         ifelse(black == 0, 0, NA)),
          other = ifelse(other == 1 & hispanic == 0, 1,
@@ -8,17 +8,21 @@ lifedata<- read.csv("./lifedata.csv") %>%
   filter(!is.na(black) & !is.na(other))
   
 
-
-htouh <- c(1:8)
+htoh <- c(1)
+htouh <- c(2:8)
 htod <- c(9)
-uhtod <- c(9*(2:8))
 uhtoh <- c(9*(1:7)+1)
 
+uhtod <- c(9*(2:8))
+
+
 lifedata <- lifedata %>%
-  mutate(trans = ifelse(trans %in% htouh,1,
-                        ifelse(trans %in% htod,2,
-                               ifelse(trans %in% uhtod, 4, 
-                                      ifelse(trans %in% uhtoh,3,NA))))) %>%
+  mutate(trans = ifelse(trans %in% htoh,1,
+                        ifelse(trans %in% htouh,2,
+                               ifelse(trans %in% htod, 3, 
+                                      ifelse(trans %in% uhtoh,4,
+                                             ifelse(trans %in% uhtod,6,
+                                                    ifelse(!is.na(trans),5,NA))))))) %>%
   filter(!is.na(trans))
 
 usethis::use_data(lifedata, overwrite = TRUE)
