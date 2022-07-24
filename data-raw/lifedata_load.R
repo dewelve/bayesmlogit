05cc1901b3aecd9aa042b14fc931b9bd5a0d87fe
Xuezhixing Zhang <xuezhixing.zhang@yale.edu>
2022-07-24 18:03:53 -0400

    updated

--- data-raw/lifedata_load.R
+++ data-raw/lifedata_load.R
@@ -4,7 +4,9 @@ lifedata<- read.csv("./lifedata.csv") %>%
   mutate(black = ifelse(black == 1 & hispanic ==0,1,
                         ifelse(black == 0, 0, 0)),
          other = ifelse(other == 1 & hispanic == 0, 1,
-                        ifelse(other==0,0,0))) %>%
+                        ifelse(other==0,0,0)))
+
+lifedata <- lifedata %>%
   filter(other !=1) %>%
   select(colnames(lifedata)[-5])
   
@@ -13,7 +15,6 @@ htoh <- c(1)
 htouh <- c(2:8)
 htod <- c(9)
 uhtoh <- c(9*(1:7)+1)
-
 uhtod <- c(9*(2:8))
 
 
