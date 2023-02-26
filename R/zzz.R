f71bb12248cffc862ad6642558d1a02ef9b2a29d
Xuezhixing Zhang <xuezhixing.zhang@yale.edu>
2023-02-25 19:03:19 -0500

    updated

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname){
-  packageStartupMessage("This package is constructed with R >= 4.0.0.","\n","Please check the vignette file 'Introductory-to-Package.html' before using this package.")
+  packageStartupMessage("This package is developed with R >= 4.0.0.","\n","Please check the vignette file 'Introductory-to-Package.html' before using this package.")
 }
